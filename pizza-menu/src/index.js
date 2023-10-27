488e9bae9a8272dd53e2d9f3e596079acc2bd952
Mopano <99732517+funnypar@users.noreply.github.com>
2023-10-27 07:26:15 -0400

    createSimplePage

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -48,7 +48,46 @@ const pizzaData = [
 ];
 // App component
 function App() {
-    return <h1>Hello from React :) </h1>;
+    return (
+        <div>
+            <Header />
+            <Menu />
+            <Footer />
+        </div>
+    );
+}
+
+function Header() {
+    return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+    return (
+        <div>
+            <h2>Our menu</h2>
+            <Pizza />
+            <Pizza />
+            <Pizza />
+        </div>
+    );
+}
+
+function Footer() {
+    return (
+        <footer>
+            {new Date().toLocaleTimeString()}. We're currently open!
+        </footer>
+    );
+}
+
+function Pizza() {
+    return (
+        <div>
+            <img src="pizzas/spinaci.jpg" alt="spinaci" />
+            <h2>Pizza Spinaci</h2>
+            <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+        </div>
+    );
 }
 // render root
 const root = ReactDOM.createRoot(document.getElementById("root"));
