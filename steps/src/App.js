aa57335b7530b2d576de1c70a7862793da481369
Mopano <99732517+funnypar@users.noreply.github.com>
2023-11-19 04:09:45 -0500

    createBox

--- steps/src/App.js
+++ steps/src/App.js
@@ -1,7 +1,30 @@
 import "./App.css";
 
 function App() {
-    return <div>Let's go !</div>;
+    return (
+        <div>
+            <Box />
+        </div>
+    );
 }
 
+function Box() {
+    return <div className="box">Hi</div>;
+}
+
+function Steps() {
+    return <ul></ul>;
+}
+
+function Texts() {
+    return <h2></h2>;
+}
+
+function Btn() {
+    return <button></button>;
+}
+
+function Close() {
+    return <div></div>;
+}
 export default App;
