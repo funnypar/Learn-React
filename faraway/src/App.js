650f712ad2ef5dd31caf0f045ab5145bb2d35648
Mopano <99732517+funnypar@users.noreply.github.com>
2023-11-23 10:56:58 -0500

    logoStyle

--- faraway/src/App.js
+++ faraway/src/App.js
@@ -1,7 +1,31 @@
 import "./App.css";
 
 function App() {
-    return <div>Hi From React</div>;
+    return (
+        <div>
+            <Header />
+            <Form />
+            <Items />
+            <Status />
+        </div>
+    );
+}
+
+function Header() {
+    return (
+        <div className="header">
+            <h1>🌵 Far Away 👜</h1>
+        </div>
+    );
+}
+function Form() {
+    return <div></div>;
+}
+function Items() {
+    return <div></div>;
+}
+function Status() {
+    return <div></div>;
 }
 
 export default App;
