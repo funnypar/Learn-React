e6aa247554cb82fae6f76c58c75d296757c988af
Mopano <99732517+funnypar@users.noreply.github.com>
2023-11-23 02:19:40 -0500

    result

--- dates/src/App.js
+++ dates/src/App.js
@@ -1,7 +1,47 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-    return <div></div>;
+    const [dateOne, setDateOne] = useState("");
+    const [dateTwo, setDateTwo] = useState("");
+
+    const dateOneHandler = (event) => {
+        setDateOne(event.target.value);
+    };
+    const dateTwoHandler = (event) => {
+        setDateTwo(event.target.value);
+    };
+
+    return (
+        <div className="app">
+            <h2>Day Counter</h2>
+            <div>
+                <label>First Data :</label>
+                <input type="date" onChange={dateOneHandler} />
+                <label>Second Data :</label>
+                <input type="date" onChange={dateTwoHandler} />
+            </div>
+            <Result dates={[dateOne, dateTwo]} />
+        </div>
+    );
 }
 
+function Result(props) {
+    let dateOne = new Date(props.dates[0]);
+    let dateTwo = new Date(props.dates[1]);
+    let status = true;
+
+    const diffTime = Math.abs(dateTwo - dateOne);
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+    if (+props.dates[0] == 0 || +props.dates[1] == 0) status = false;
+
+    return (
+        <h3>
+            {status
+                ? `The Difference between 2 Dates is :  ${diffDays}`
+                : `Please Insert Both Of Dates`}
+        </h3>
+    );
+}
 export default App;
