c052563494a52a1445d47949a672d955ede9b196
Mopano <99732517+funnypar@users.noreply.github.com>
2024-01-07 04:01:49 -0500

    CreateOneCard

--- flashcard/src/App.js
+++ flashcard/src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
 
+const DATABASE = [{"How are you ?": "Good"}];
+
 function App() {
   return(
-    <div>Hello new project !</div>
+    <div className='App'>
+        <FlashCard />
+    </div>
   )
+}
+
+function FlashCard() {
+    return(
+        <div className='card-wrapper'>
+            {DATABASE.map(el => {
+                return <Card data={el}/>
+            })}
+        </div>
+    )
+}
+
+function Card({data}) {
+    return(
+        <div className='card'>{Object.keys(data)}</div>
+    )
 }
 
 export default App;
