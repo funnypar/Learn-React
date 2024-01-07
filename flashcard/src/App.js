26ec1138c86bb9d73ccfd3a7aeccf48eb3e0599f
Mopano <99732517+funnypar@users.noreply.github.com>
2024-01-07 10:13:05 -0500

    final

--- flashcard/src/App.js
+++ flashcard/src/App.js
@@ -1,6 +1,12 @@
+import {useState} from "react";
 import './App.css';
 
-const DATABASE = [{"How are you ?": "Good"}];
+const DATABASE = [{"What language is React based on ?": "Javascript"},
+{"What are the building blocks of React apps ?": "Components"}, 
+{"What's the name of the syntax we use to describe a UI in React ?" : "JSX"},
+{"How to pass data from parent to child components ? " : "Props"},
+{"How to give components memory ?" : "State"}, 
+{"What do we call an input element that is completely synchronised with state ?" : "Google it ;)"}];
 
 function App() {
   return(
@@ -21,8 +27,25 @@ function FlashCard() {
 }
 
 function Card({data}) {
+    let [active, onActive] = useState(true)
+    function answer() {
+        if (active == true) {
+            onActive(false);
+        }else {
+            onActive(true);
+        }
+    }
     return(
-        <div className='card'>{Object.keys(data)}</div>
+        <div className='card' onClick={answer}>
+            <div class={`card-body ${active ? '' : 'card-rotate'}`}>
+                <div class="card-front">
+                    <p>{Object.keys(data)}</p>
+                </div>
+                <div class="card-back">
+                    <p>{Object.values(data)}</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
