572b8502cb482d973990acdcfc6ec3f2b4e59c20
Mopano <99732517+funnypar@users.noreply.github.com>
2024-01-11 13:48:52 -0500

    DateCalculations

--- dayCounter/src/App.js
+++ dayCounter/src/App.js
@@ -1,34 +1,52 @@
 import {useState} from "react";
 import './App.css';
 
+const DATABASE = [{"What language is React based on ?": "Javascript"},
+{"What are the building blocks of React apps ?": "Components"}, 
+{"What's the name of the syntax we use to describe a UI in React ?" : "JSX"},
+{"How to pass data from parent to child components ? " : "Props"},
+{"How to give components memory ?" : "State"}, 
+{"What do we call an input element that is completely synchronised with state ?" : "Google it ;)"}];
+
 function App() {
   return(
     <div className='App'>
-        <h1>Day Counter</h1>
-        <Box />
+        <FlashCard />
     </div>
   )
 }
 
-function Box() {
+function FlashCard() {
     return(
-        <div className='box'>
-            <form >
-                <div>
-                    <p>7</p>
-                    <input type="range" min={1} max={10} />
-                </div>
-                <div>
-                    <button className="btn-small">&#60;</button>
-                    <input min={1}/>
-                    <button className="btn-small">&gt;</button>
-                </div>
-                <p>Hello</p>
-                <button className="btn">Reset</button>
-            </form>
+        <div className='card-wrapper'>
+            {DATABASE.map(el => {
+                return <Card data={el}/>
+            })}
         </div>
     )
 }
 
+function Card({data}) {
+    let [active, onActive] = useState(true)
+    function answer() {
+        if (active == true) {
+            onActive(false);
+        }else {
+            onActive(true);
+        }
+    }
+    return(
+        <div className='card' onClick={answer}>
+            <div class={`card-body ${active ? '' : 'card-rotate'}`}>
+                <div class="card-front">
+                    <p>{Object.keys(data)}</p>
+                </div>
+                <div class="card-back">
+                    <p>{Object.values(data)}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
 
 export default App;
