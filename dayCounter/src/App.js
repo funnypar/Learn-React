0937f3b49e5697691e2ecdae071f97518f7cefa2
Mopano <99732517+funnypar@users.noreply.github.com>
2024-01-12 16:20:24 -0500

    periodHandler

--- dayCounter/src/App.js
+++ dayCounter/src/App.js
@@ -1,52 +1,80 @@
 import {useState} from "react";
 import './App.css';
 
-const DATABASE = [{"What language is React based on ?": "Javascript"},
-{"What are the building blocks of React apps ?": "Components"}, 
-{"What's the name of the syntax we use to describe a UI in React ?" : "JSX"},
-{"How to pass data from parent to child components ? " : "Props"},
-{"How to give components memory ?" : "State"}, 
-{"What do we call an input element that is completely synchronised with state ?" : "Google it ;)"}];
-
 function App() {
   return(
     <div className='App'>
-        <FlashCard />
+        <h1>Day Counter</h1>
+        <Box />
     </div>
   )
 }
 
-function FlashCard() {
+function Box() {
+    const [count,setCount] = useState(0);
+    const [period,setPeriod] = useState(1);
+    // handle Error
+    if (count < 0) {
+        setCount(0)
+        alert("Please insert positive number :)")
+    }
+    // -------------- Dates ----------------------------------------
+    const date1 = new Date();
+    // calculation for date 2
+    date1.setDate(date1.getDate() + count);
+
+    function inputHandler(event) {
+        setCount(+event.target.value);
+    }
+    // ------------ handle content ---------------------------------
+    let content = `${count} days after today is ${date1.toDateString()}`;
+    if (count == 0) {
+        content = "Today is a gift :)";
+    } 
+    // ------------ handlers  ----------------------------------- 
+    function prevClickHandler(event) {
+        event.preventDefault();
+        if (period == 1) {
+            setCount(count - 1)
+        }else {
+            setCount(count - period)
+        }
+    }
+    function nextClickHandler(event) {
+        event.preventDefault();
+        if (period == 1) {
+            setCount(count + 1)
+        }else {
+            setCount(count + period)
+        }
+    }
+    function periodHandler(event) {
+        setPeriod(+event.target.value);
+    }
+    function resetHandler(event) {
+        event.preventDefault();
+        setCount(0)
+        setPeriod(1)
+    }
+    // ------------ Return JSX ------------------------------------ 
     return(
-        <div className='card-wrapper'>
-            {DATABASE.map(el => {
-                return <Card data={el}/>
-            })}
+        <div className='box'>
+            <form >
+                <div>
+                    <p>The period : {period}</p>
+                    <input type="range" min={1} max={10} value={period} onChange={periodHandler}/>
+                </div>
+                <div>
+                    <button className="btn-small" onClick={prevClickHandler}>&#60;</button>
+                    <input type="text" min={1} value={count} onChange={inputHandler}/>
+                    <button className="btn-small" onClick={nextClickHandler}>&gt;</button>
+                </div>
+                <p>{content}</p>
+                <button className="btn" onClick={resetHandler}>Reset</button>
+            </form>
         </div>
     )
 }
 
-function Card({data}) {
-    let [active, onActive] = useState(true)
-    function answer() {
-        if (active == true) {
-            onActive(false);
-        }else {
-            onActive(true);
-        }
-    }
-    return(
-        <div className='card' onClick={answer}>
-            <div class={`card-body ${active ? '' : 'card-rotate'}`}>
-                <div class="card-front">
-                    <p>{Object.keys(data)}</p>
-                </div>
-                <div class="card-back">
-                    <p>{Object.values(data)}</p>
-                </div>
-            </div>
-        </div>
-    )
-}
 
 export default App;
