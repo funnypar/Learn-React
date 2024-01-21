1e4f107c99f3c12ca0986b29ca6d09ec8c7fea19
Mopano <99732517+funnypar@users.noreply.github.com>
2024-01-21 11:27:37 -0500

    handleItem

--- accordion/src/components/Item.js
+++ accordion/src/components/Item.js
@@ -1,27 +1,56 @@
+import { useState } from "react";
+
 export default function Item() {
+    const [show, setShow] = useState(false);
+
+    function showHalndelr() {
+        setShow(!show);
+    }
+
     return (
-        <div className={"item"}>
+        <div className={"item"} onClick={showHalndelr}>
             <div className="header-wrapper">
                 <h3>What is React ?</h3>
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                >
-                    <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="m19.5 8.25-7.5 7.5-7.5-7.5"
-                    />
-                </svg>
+                {!show ? (
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                    >
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="m19.5 8.25-7.5 7.5-7.5-7.5"
+                        />
+                    </svg>
+                ) : (
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-6 h-6"
+                    >
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M5 12h14"
+                        />
+                    </svg>
+                )}
             </div>
-            <p>
-                React is a JavaScript library created by Facebook React is a
-                User Interface (UI) library React is a tool for building UI
-                components
-            </p>
+            {show ? (
+                <p>
+                    React is a JavaScript library created by Facebook React is a
+                    User Interface (UI) library React is a tool for building UI
+                    components
+                </p>
+            ) : (
+                ""
+            )}
         </div>
     );
 }
