ba3ba6356d6510a777ad7ddd6386952524a9b17f
Mopano <99732517+funnypar@users.noreply.github.com>
2024-01-21 08:33:45 -0500

    handleItems

--- accordion/src/App.js
+++ accordion/src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Item from "./components/Item";
 
 function App() {
-    return <div className="App">hi</div>;
+    return (
+        <div className="App">
+            <Item />
+            <Item />
+            <Item />
+        </div>
+    );
 }
 
 export default App;
---
+++ accordion/src/components/Item.js
@@ -0,0 +1,12 @@
+export default function Item() {
+    return (
+        <div className={"item"}>
+            <h3>What is React ?</h3>
+            <p>
+                React is a JavaScript library created by Facebook React is a
+                User Interface (UI) library React is a tool for building UI
+                components
+            </p>
+        </div>
+    );
+}
