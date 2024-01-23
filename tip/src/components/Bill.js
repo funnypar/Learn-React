c83d668f9fd32986274afd34dfb3ba011c72ab1a
Mopano <99732517+funnypar@users.noreply.github.com>
2024-01-23 09:02:53 -0500

    styledApp

--- tip/src/components/Bill.js
+++ tip/src/components/Bill.js
@@ -1,8 +1,8 @@
 export default function Bill() {
     return (
-        <>
-            <input id="bill" type="number" />
+        <div>
             <label for="bill">How much is your bill ? </label>
-        </>
+            <input id="bill" type="number" placeholder="100" />
+        </div>
     );
 }
