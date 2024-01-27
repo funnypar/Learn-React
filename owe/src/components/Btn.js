d7983ff947bf1de71b0f82701cb6fe1d21a74922
Mopano <99732517+funnypar@users.noreply.github.com>
2024-01-27 04:56:44 -0500

    implementLogicAddFriend

--- owe/src/components/Btn.js
+++ owe/src/components/Btn.js
@@ -1,3 +1,11 @@
-export default function Btn({ children, classname }) {
-    return <button className={`${classname}`}>{children}</button>;
+export default function Btn({ children, classname, onClicked }) {
+    function addFriendHandler() {
+        onClicked(true);
+    }
+
+    return (
+        <button className={`${classname}`} onClick={addFriendHandler}>
+            {children}
+        </button>
+    );
 }
--- owe/src/components/Friends.js
+++ owe/src/components/Friends.js
@@ -1,6 +1,7 @@
 import Friend from "./Friend";
 import Btn from "./Btn";
 import Form from "./Form";
+import { useState } from "react";
 
 let DATABASE = [
     { id: "1p", name: "sara" },
@@ -9,14 +10,34 @@ let DATABASE = [
 ];
 
 export default function Friends() {
+    const [addFriend, setAddFriend] = useState(false);
+
     return (
         <div className="wrapper">
             {DATABASE.map((person) => {
                 return <Friend name={person.name} key={person.id} />;
             })}
-            <Btn classname={"btn-add btn"}>Add Friend</Btn>
-            <Form />
-            <Btn classname={"btn-close btn"}>Close</Btn>
+            {!addFriend ? (
+                <Btn
+                    classname={"btn-add btn"}
+                    onClicked={() => setAddFriend(!addFriend)}
+                >
+                    Add Friend
+                </Btn>
+            ) : (
+                ""
+            )}
+            {addFriend ? <Form /> : ""}
+            {addFriend ? (
+                <Btn
+                    classname={"btn-close btn"}
+                    onClicked={() => setAddFriend(!addFriend)}
+                >
+                    Close
+                </Btn>
+            ) : (
+                ""
+            )}
         </div>
     );
 }
