9b6530d9638fb66178c9e1db2e58846e2ffa9b99
Mopano <99732517+funnypar@users.noreply.github.com>
2024-01-30 03:28:42 -0500

    added

--- films/src/App.js
+++ films/src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
+import List from "./components/List";
 import Nav from "./components/Nav";
+
 function App() {
     return (
         <div className="App">
             <Nav />
+            <div className="wrapper">
+                <List />
+                <List />
+            </div>
         </div>
     );
 }
