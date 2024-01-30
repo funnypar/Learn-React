ba22cb14bbab68cc0e8d7dd1f20b0803802049fe
Mopano <99732517+funnypar@users.noreply.github.com>
2024-01-30 02:37:00 -0500

    addedNav

---
+++ films/src/components/Nav.js
@@ -0,0 +1,15 @@
+export default function Nav() {
+    return (
+        <nav>
+            <div>
+                <img
+                    src="media/mopanofilms-high-resolution-logo-transparent.png"
+                    alt="logo"
+                />
+                <h2>Mopanofilms</h2>
+            </div>
+            <input type="text" placeholder="Search films..." />
+            <h3>Found 0 top results</h3>
+        </nav>
+    );
+}
