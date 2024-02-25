5f29aaed956da0859a7cca599e8ebd0edaee8f0b
Mopano <99732517+funnypar@users.noreply.github.com>
2024-02-25 04:09:26 -0500

    abortRequests

--- films/src/App.js
+++ films/src/App.js
@@ -20,25 +20,32 @@ function App() {
 
     useEffect(
         function () {
+            const controller = new AbortController();
             async function fetchData() {
                 try {
                     setErr("");
                     setIsLoad(true);
                     if (!query) throw new Error("🎥 Search for a movie");
                     const req = await fetch(
-                        `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+                        `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+                        { signal: controller.signal }
                     );
                     const data = await req.json();
                     if (data.Response === "False")
                         throw new Error("🥹 Something went wrong...");
                     setDATBASE(data.Search);
                     setIsLoad(false);
+                    setErr("");
                 } catch (err) {
-                    console.log(err.message);
-                    setErr(err.message);
+                    if (err.name !== "AbortError") {
+                        console.log(err.message);
+                        setErr(err.message);
+                    }
                 }
             }
             fetchData();
+
+            return () => controller.abort();
         },
         [query]
     );
