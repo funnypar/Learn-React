6b177295dbe1afd05eadaf3e1c81744ef9dcdf00
Mopano <99732517+funnypar@users.noreply.github.com>
2024-02-29 10:29:48 -0500

    EnterLogic

--- films/src/components/Search.js
+++ films/src/components/Search.js
@@ -1,16 +1,41 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Search({ onSearch }) {
     const el = useRef(null);
+    const [query, setQuery] = useState("");
+
+    function changeHandler(event) {
+        setQuery(event.target.value);
+        onSearch(event.target.value);
+    }
 
     useEffect(() => el.current.focus(), []);
+
+    useEffect(
+        function () {
+            function callback(e) {
+                if (document.activeElement === el.current) return;
+                if (e.code === "Enter") {
+                    el.current.focus();
+                    onSearch("");
+                    setQuery("");
+                }
+            }
+
+            document.addEventListener("keydown", callback);
+
+            return document.addEventListener("keydown", callback);
+        },
+        [onSearch]
+    );
 
     return (
         <input
             type="text"
             placeholder="Search films..."
-            onChange={(event) => onSearch(event.target.value)}
+            onChange={changeHandler}
             className="search"
+            value={query}
             ref={el}
         />
     );
