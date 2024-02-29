1e166adfb35da1af8756591c332619e93e33c10f
Mopano <99732517+funnypar@users.noreply.github.com>
2024-02-29 10:07:46 -0500

    UseInputWhenMount

--- films/src/components/Search.js
+++ films/src/components/Search.js
@@ -1,9 +1,17 @@
+import { useEffect, useRef } from "react";
+
 export default function Search({ onSearch }) {
+    const el = useRef(null);
+
+    useEffect(() => el.current.focus(), []);
+
     return (
         <input
             type="text"
             placeholder="Search films..."
             onChange={(event) => onSearch(event.target.value)}
+            className="search"
+            ref={el}
         />
     );
 }
