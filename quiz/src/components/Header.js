7be79fbb9bc2fb6a906be2e014d6907f2df41d45
Mopano <99732517+funnypar@users.noreply.github.com>
2024-03-03 16:47:44 +0330

    implementHeader

---
+++ quiz/src/components/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <div className="header">
+            <img src="media/ball.png" alt="ball" />
+            <h1>The Football Quiz</h1>
+        </div>
+    );
+}
