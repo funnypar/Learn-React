68893502b47a55d468ef51b3a54532d76a6053ea
Mopano <99732517+funnypar@users.noreply.github.com>
2024-03-03 18:10:00 +0330

    implementQuestions

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Bar from "./components/Bar";
 import Header from "./components/Header";
+import Questions from "./components/Questions";
 
 function App() {
     return (
         <div className="App">
             <Header />
             <Bar />
+            <Questions />
         </div>
     );
 }
---
+++ quiz/src/components/Questions.js
@@ -0,0 +1,14 @@
+export default function Questions() {
+    return (
+        <div className="questions">
+            <h3>Who is the best player in the history? </h3>
+            <ul>
+                <li>Ronaldo</li>
+                <li>Messi</li>
+                <li>Maradona</li>
+                <li>Pele</li>
+            </ul>
+            <div className="time">10:00</div>
+        </div>
+    );
+}
