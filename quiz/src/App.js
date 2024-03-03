e2a1e7d3276396334e3f101b5016d27e8b41f7d4
Mopano <99732517+funnypar@users.noreply.github.com>
2024-03-03 17:46:46 +0330

    impelementBar

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Bar from "./components/Bar";
 import Header from "./components/Header";
 
 function App() {
     return (
         <div className="App">
             <Header />
+            <Bar />
         </div>
     );
 }
---
+++ quiz/src/components/Bar.js
@@ -0,0 +1,11 @@
+export default function Bar() {
+    return (
+        <div className="bar">
+            <input type="range" />
+            <div className="bar-infos">
+                <p>Question 2/15</p>
+                <p>10/150 Points</p>
+            </div>
+        </div>
+    );
+}
