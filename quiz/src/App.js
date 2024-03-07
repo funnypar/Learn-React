b982f5fb1a1db01dc65a2fc32978d3a13a76b9b7
Mopano <99732517+funnypar@users.noreply.github.com>
2024-03-07 20:27:19 +0330

    app

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -5,13 +5,17 @@ import Loader from "./components/Loader";
 import Ready from "./components/Ready";
 import Error from "./components/Error";
 import Questions from "./components/Questions";
+import Finish from "./components/Finish";
 
+const SEC_PER_QUESTION = 20;
 const initialState = {
     question: [],
     status: "loading",
     index: 0,
     score: 0,
     answer: null,
+    highscore: 0,
+    seconds: null,
 };
 function reducer(state, action) {
     switch (action.type) {
@@ -20,7 +24,11 @@ function reducer(state, action) {
         case "ok":
             return { ...state, questions: action.payload, status: "ok" };
         case "start":
-            return { ...state, status: "start" };
+            return {
+                ...state,
+                status: "start",
+                seconds: state.questions.length * SEC_PER_QUESTION,
+            };
         case "next":
             return { ...state, index: action.payload, answer: null };
         case "newAnswer":
@@ -33,6 +41,30 @@ function reducer(state, action) {
                     state.questions[state.index].correctOption
                         ? lastScore + 10
                         : lastScore,
+            };
+        case "finish":
+            return {
+                ...state,
+                status: "finish",
+                highscore:
+                    state.score > state.highscore
+                        ? state.score
+                        : state.highscore,
+            };
+        case "restart":
+            return {
+                ...state,
+                status: "start",
+                index: 0,
+                score: 0,
+                answer: null,
+                seconds: state.questions.length * SEC_PER_QUESTION,
+            };
+        case "seconds":
+            return {
+                ...state,
+                seconds: state.seconds--,
+                status: state.seconds === 0 ? "finish" : state.status,
             };
         default:
             throw new Error("Action unknown");
@@ -60,7 +92,12 @@ function App() {
         <div className="App">
             <Header />
             {state.status === "loading" && <Loader />}
-            {state.status === "ok" && <Ready dispatch={dispatch} />}
+            {state.status === "ok" && (
+                <Ready
+                    dispatch={dispatch}
+                    numQuestions={state.questions.length}
+                />
+            )}
             {state.status === "error" && <Error message={state.message} />}
             {state.status === "start" && (
                 <Questions
@@ -69,6 +106,15 @@ function App() {
                     dispatch={dispatch}
                     answer={state.answer}
                     score={state.score}
+                    seconds={state.seconds}
+                />
+            )}
+            {state.status === "finish" && (
+                <Finish
+                    score={state.score}
+                    numQuestions={state.questions.length}
+                    highscore={state.highscore}
+                    dispatch={dispatch}
                 />
             )}
         </div>
