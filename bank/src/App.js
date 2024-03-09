8ef2c0da943167715e4c5caef21bddbb57cbe75a
Mopano <99732517+funnypar@users.noreply.github.com>
2024-03-09 11:07:18 +0330

    appStyled

--- bank/src/App.js
+++ bank/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-    return <div className="App">hi</div>;
+    return (
+        <div className="App">
+            <h1>🏦 Bank</h1>
+            <div className="box"></div>
+        </div>
+    );
 }
 
 export default App;
