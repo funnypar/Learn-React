a43489b12f1fba506d5729ff17dd4b78015b8398
Mopano <99732517+funnypar@users.noreply.github.com>
2024-03-12 23:03:28 +0330

    addedLogin

--- worldTrip/src/pages/Login.jsx
+++ worldTrip/src/pages/Login.jsx
@@ -1,3 +1,21 @@
+import styles from "./Login.module.css";
+import Nav from "../components/Nav";
+
 export default function Login() {
-    return <h1>Login</h1>;
+    return (
+        <div className={styles.wrapper}>
+            <Nav />
+            <form>
+                <div>
+                    <label>Email :</label>
+                    <input type="email" required />
+                </div>
+                <div>
+                    <label>Password :</label>
+                    <input type="password" required />
+                </div>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
 }
