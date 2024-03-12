a6e7cdd833ec1f942fe5145d9d75ebc199288c62
Mopano <99732517+funnypar@users.noreply.github.com>
2024-03-12 23:38:26 +0330

    addedPricing

--- worldTrip/src/pages/Pricing.jsx
+++ worldTrip/src/pages/Pricing.jsx
@@ -1,3 +1,25 @@
+import Nav from "../components/Nav";
+import styles from "./Pricing.module.css";
+
 export default function Pricing() {
-    return <div>hi</div>;
+    return (
+        <div className={styles.wrapper}>
+            <Nav />
+            <div className={styles.parts}>
+                <img src="/media/horizon.jpg" alt="sunset" />
+                <div className={styles.infos}>
+                    <h3>
+                        Simple pricing
+                        <br />
+                        Just $9/month
+                    </h3>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Vitae vel labore mollitia iusto. Recusandae quos
+                        provident, laboriosam fugit voluptatem iste.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
 }
