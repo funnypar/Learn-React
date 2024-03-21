eafcfb86cee551606ae38f5e8c452117dc6d55cb
Mopano <99732517+funnypar@users.noreply.github.com>
2024-03-21 13:04:03 +0330

    mapMarker

--- worldTrip/src/components/Map.jsx
+++ worldTrip/src/components/Map.jsx
@@ -2,8 +2,10 @@ import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 // import { useNavigate } from "react-router-dom";
 import styles from "./Map.module.css";
 import { useState } from "react";
+import { useCities } from "../contexts/CitiesProvider";
 
 export default function Map() {
+    const { cities } = useCities();
     // eslint-disable-next-line no-unused-vars
     const [mapPosition, setMapPosition] = useState([40, 0]);
     // const navigate = useNavigate();
@@ -21,11 +23,19 @@ export default function Map() {
                 attribution='&copy; <a href="https://www.openstreetmap.fr/h/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={mapPosition}>
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
+            {cities.map((city) => (
+                <Marker
+                    position={[city.position.lat, city.position.lng]}
+                    key={city.id}
+                >
+                    <Popup>
+                        <p>
+                            <span>{city.emoji}</span>
+                            {city.cityName}
+                        </p>
+                    </Popup>
+                </Marker>
+            ))}
         </MapContainer>
     );
 }
