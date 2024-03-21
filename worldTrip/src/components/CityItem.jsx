303dc90fd06663ad0b6ede6384dd662b0cd7bbf0
Mopano <99732517+funnypar@users.noreply.github.com>
2024-03-21 10:42:15 +0330

    activeCityStyle

--- worldTrip/src/components/CityItem.jsx
+++ worldTrip/src/components/CityItem.jsx
@@ -2,8 +2,11 @@
 import { Link } from "react-router-dom";
 import styles from "./CityItem.module.css";
 import Btn from "./Btn";
+import { useCities } from "../contexts/CitiesProvider";
 
 export default function CityItem({ data }) {
+    const { currentCity } = useCities();
+    console.log(data.id, currentCity.id);
     function dateFormat(date) {
         return new Intl.DateTimeFormat("en", {
             day: "numeric",
@@ -16,7 +19,11 @@ export default function CityItem({ data }) {
             to={`${data.id}?lat=${data.position.lat}&lng=${data.position.lng}`}
             className={styles.wrapper}
         >
-            <div className={styles.city}>
+            <div
+                className={`${styles.city} ${
+                    currentCity.id === data.id ? styles.activeCity : ""
+                }`}
+            >
                 <p>
                     <span>{data.emoji}</span> {data.cityName}
                 </p>
