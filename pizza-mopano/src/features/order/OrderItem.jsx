5782ccf14249bdd753b5b3b33399eddd0478ea28
Mopano <99732517+funnypar@users.noreply.github.com>
2024-03-29 20:02:19 +0330

    fetchMenu

--- pizza-mopano/src/features/order/OrderItem.jsx
+++ pizza-mopano/src/features/order/OrderItem.jsx
@@ -1,16 +1,20 @@
-function OrderItem({ item, isLoadingIngredients, ingredients }) {
-  const { quantity, name, totalPrice } = item;
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import { formatCurrency } from "../../utilities/helpers";
 
-  return (
-    <li>
-      <div>
-        <p>
-          <span>{quantity}&times;</span> {name}
-        </p>
-        <p>{formatCurrency(totalPrice)}</p>
-      </div>
-    </li>
-  );
+function OrderItem({ item, isLoadingIngredients, ingredients }) {
+    const { quantity, name, totalPrice } = item;
+
+    return (
+        <li>
+            <div>
+                <p>
+                    <span>{quantity}&times;</span> {name}
+                </p>
+                <p>{formatCurrency(totalPrice)}</p>
+            </div>
+        </li>
+    );
 }
 
 export default OrderItem;
