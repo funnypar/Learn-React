3c1fc3f269cf8d4d2a1783215325fc61ded97fe0
Mopano <99732517+funnypar@users.noreply.github.com>
2024-03-29 15:28:04 +0330

    appLayout

--- pizza-mopano/src/App.jsx
+++ pizza-mopano/src/App.jsx
@@ -1,7 +1,41 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
+import AppLayout from "./UI/AppLayout";
+import Home from "./UI/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
 
+const router = createBrowserRouter([
+    {
+        element: <AppLayout />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+            {
+                path: "/menu",
+                element: <Menu />,
+            },
+            {
+                path: "/cart",
+                element: <Cart />,
+            },
+            {
+                path: "/order/new",
+                element: <CreateOrder />,
+            },
+            {
+                path: "/order/:orderId",
+                element: <Order />,
+            },
+        ],
+    },
+]);
 function App() {
-    return <div>Start</div>;
+    return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ pizza-mopano/src/UI/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Header = () => {
+    return (
+        <header>
+            <Link to="/">Pizza Mopano Co.</Link>
+            <p>Parsa</p>
+        </header>
+    );
+};
+
+export default Header;
