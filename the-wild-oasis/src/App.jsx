e103a2948648ba2034b301ae5563ecbfbd8359d4
Mopano <99732517+funnypar@users.noreply.github.com>
2024-04-09 15:05:09 +0330

    createPagesRoutes

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -1,10 +1,34 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Account from "./pages/Account";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Login from "./pages/Login";
+import Setting from "./pages/Settings";
+import Users from "./pages/Users";
+import PageNotFound from "./pages/PageNotFound";
 import GlobalStyle from "./styles/GlobalStyled";
 
 function App() {
     return (
         <>
             <GlobalStyle />
-            <div>Hello</div>
+            <BrowserRouter>
+                <Routes>
+                    <Route
+                        index
+                        element={<Navigate replace to="dashboard" />}
+                    />
+                    <Route path="dashboard" element={<Dashboard />} />
+                    <Route path="account" element={<Account />} />
+                    <Route path="booking" element={<Bookings />} />
+                    <Route path="cabins" element={<Cabins />} />
+                    <Route path="login" element={<Login />} />
+                    <Route path="setting" element={<Setting />} />
+                    <Route path="users" element={<Users />} />
+                    <Route path="*" element={<PageNotFound />} />
+                </Routes>
+            </BrowserRouter>
         </>
     );
 }
