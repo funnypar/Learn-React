f6bf0a9df16ae2a17ef93f18486eca5e7c0eb0a7
Mopano <99732517+funnypar@users.noreply.github.com>
2024-04-10 11:28:07 +0330

    FinishAppLayout

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -23,9 +23,9 @@ function App() {
                         />
                         <Route path="dashboard" element={<Dashboard />} />
                         <Route path="account" element={<Account />} />
-                        <Route path="booking" element={<Bookings />} />
+                        <Route path="bookings" element={<Bookings />} />
                         <Route path="cabins" element={<Cabins />} />
-                        <Route path="setting" element={<Setting />} />
+                        <Route path="settings" element={<Setting />} />
                         <Route path="users" element={<Users />} />
                     </Route>
                     <Route path="login" element={<Login />} />
