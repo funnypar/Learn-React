dcd11ba1fc164d748efb205d04cd59d25c97e49b
Mopano <99732517+funnypar@users.noreply.github.com>
2024-04-11 16:07:38 +0330

    addedCabinsTable

--- the-wild-oasis/src/services/apiCabins.js
+++ the-wild-oasis/src/services/apiCabins.js
@@ -3,9 +3,7 @@ import supabase from "./supabase";
 export async function getCabins() {
     const { data, error } = await supabase.from("cabins").select("*");
 
-    if (!error) {
-        console.log(data);
-    } else {
+    if (error) {
         console.log(error);
         throw new Error("Could not read Cabins");
     }
