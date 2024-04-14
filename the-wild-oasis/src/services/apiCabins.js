3d0cccddc48ca6219567d8f0e30c8fa9a4c68e0d
Mopano <99732517+funnypar@users.noreply.github.com>
2024-04-14 22:59:52 +0330

    addedCreateCabin

--- the-wild-oasis/src/services/apiCabins.js
+++ the-wild-oasis/src/services/apiCabins.js
@@ -11,11 +11,31 @@ export async function getCabins() {
     return data;
 }
 
+export async function createCabin(newCabin) {
+    const { data, error } = await supabase
+        .from("cabins")
+        .insert([
+            {
+                name: newCabin.name,
+                "max-capacity": +newCabin.maxCapacity,
+                "regular-price": +newCabin.regularPrice,
+                discount: +newCabin.discount,
+                description: newCabin.description,
+            },
+        ])
+        .select();
+    if (error) {
+        console.log(error);
+        throw new Error("Could not create Cabins");
+    }
+
+    return data;
+}
 export async function deleteCabins(id) {
     const { error } = await supabase.from("cabins").delete().eq("id", id);
 
     if (error) {
         console.log(error);
-        throw new Error("Could not read Cabins");
+        throw new Error("Could not delete Cabins");
     }
 }
