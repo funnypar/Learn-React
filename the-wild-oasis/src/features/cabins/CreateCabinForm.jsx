a1c2f4662b4d4d8ee88dd5f59682839c27563fa8
Mopano <99732517+funnypar@users.noreply.github.com>
2024-04-15 10:36:18 +0330

    handleValidationForm

--- the-wild-oasis/src/features/cabins/CreateCabinForm.jsx
+++ the-wild-oasis/src/features/cabins/CreateCabinForm.jsx
@@ -46,7 +46,8 @@ const Error = styled.span`
 `;
 
 function CreateCabinForm() {
-    const { register, handleSubmit, reset } = useForm();
+    const { register, handleSubmit, reset, getValues, formState } = useForm();
+    const { errors } = formState;
     const queryClient = useQueryClient();
 
     const { isLoading: isCreating, mutate } = useMutation({
@@ -69,7 +70,14 @@ function CreateCabinForm() {
         <Form onSubmit={handleSubmit(submitHandler)}>
             <FormRow>
                 <Label htmlFor="name">Cabin name</Label>
-                <Input type="text" id="name" {...register("name")} />
+                <Input
+                    type="text"
+                    id="name"
+                    {...register("name", {
+                        required: "This filed is required!",
+                    })}
+                />
+                {errors?.name?.message && <Error>{errors.name.message}</Error>}
             </FormRow>
 
             <FormRow>
@@ -77,8 +85,13 @@ function CreateCabinForm() {
                 <Input
                     type="number"
                     id="maxCapacity"
-                    {...register("maxCapacity")}
+                    {...register("maxCapacity", {
+                        required: "This filed is required!",
+                    })}
                 />
+                {errors?.maxCapacity?.message && (
+                    <Error>{errors.maxCapacity.message}</Error>
+                )}
             </FormRow>
 
             <FormRow>
@@ -86,8 +99,13 @@ function CreateCabinForm() {
                 <Input
                     type="number"
                     id="regularPrice"
-                    {...register("regularPrice")}
+                    {...register("regularPrice", {
+                        required: "This filed is required!",
+                    })}
                 />
+                {errors?.regularPrice?.message && (
+                    <Error>{errors.regularPrice.message}</Error>
+                )}
             </FormRow>
 
             <FormRow>
@@ -96,8 +114,16 @@ function CreateCabinForm() {
                     type="number"
                     id="discount"
                     defaultValue={0}
-                    {...register("discount")}
+                    {...register("discount", {
+                        required: "This filed is required!",
+                        validate: (value) =>
+                            value <= getValues().regularPrice ||
+                            "The discount must be smaller than the price",
+                    })}
                 />
+                {errors?.discount?.message && (
+                    <Error>{errors.discount.message}</Error>
+                )}
             </FormRow>
 
             <FormRow>
@@ -106,8 +132,13 @@ function CreateCabinForm() {
                     type="number"
                     id="description"
                     defaultValue=""
-                    {...register("description")}
+                    {...register("description", {
+                        required: "This filed is required!",
+                    })}
                 />
+                {errors?.description?.message && (
+                    <Error>{errors.description.message}</Error>
+                )}
             </FormRow>
 
             <FormRow>
