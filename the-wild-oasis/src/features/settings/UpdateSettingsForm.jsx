e1381c1ac793dff27952ca256e49f34d1b9d8416
Mopano <99732517+funnypar@users.noreply.github.com>
2024-04-18 18:29:56 +0330

    addedUseSettings

--- the-wild-oasis/src/features/settings/UpdateSettingsForm.jsx
+++ the-wild-oasis/src/features/settings/UpdateSettingsForm.jsx
@@ -1,24 +1,54 @@
-import Form from '../../ui/Form';
-import FormRow from '../../ui/FormRow';
-import Input from '../../ui/Input';
+import Form from "../../ui/Form";
+import FormRow from "../../ui/FormRow";
+import Spinner from "../../ui/Spinner";
+import Input from "../../ui/Input";
+import { useSettings } from "./useSettings";
 
 function UpdateSettingsForm() {
-  return (
-    <Form>
-      <FormRow label='Minimum nights/booking'>
-        <Input type='number' id='min-nights' />
-      </FormRow>
-      <FormRow label='Maximum nights/booking'>
-        <Input type='number' id='max-nights' />
-      </FormRow>
-      <FormRow label='Maximum guests/booking'>
-        <Input type='number' id='max-guests' />
-      </FormRow>
-      <FormRow label='Breakfast price'>
-        <Input type='number' id='breakfast-price' />
-      </FormRow>
-    </Form>
-  );
+    const {
+        isLoading,
+        error,
+        settings: {
+            "min-booking-length": minBookingLength,
+            "max-booking-length": maxBookingLength,
+            "max-guests-per-booking": maxGuestsPerBooking,
+            "breakfast-price": breakfastPrice,
+        } = {},
+    } = useSettings();
+
+    if (isLoading) return <Spinner />;
+    return (
+        <Form>
+            <FormRow label="Minimum nights/booking">
+                <Input
+                    type="number"
+                    id="min-nights"
+                    defaultValue={minBookingLength}
+                />
+            </FormRow>
+            <FormRow label="Maximum nights/booking">
+                <Input
+                    type="number"
+                    id="max-nights"
+                    defaultValue={maxBookingLength}
+                />
+            </FormRow>
+            <FormRow label="Maximum guests/booking">
+                <Input
+                    type="number"
+                    id="max-guests"
+                    defaultValue={maxGuestsPerBooking}
+                />
+            </FormRow>
+            <FormRow label="Breakfast price">
+                <Input
+                    type="number"
+                    id="breakfast-price"
+                    defaultValue={breakfastPrice}
+                />
+            </FormRow>
+        </Form>
+    );
 }
 
 export default UpdateSettingsForm;
